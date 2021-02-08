90373a2189de1339a95afb8833d99a4370d9bc36
Andrew Poženel - SDT <22171451+anderlli0053@users.noreply.github.com>
2021-02-08 01:54:12 +0100

    Add files via upload

--- addons/SDT_Importer/plugin.gd
+++ addons/SDT_Importer/plugin.gd
@@ -1,3 +1,10 @@
+# Copyright © 2020 FULL NAME and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+#
+# This script is needed to make the `class_name` scripts visible in the
+# Create New Node dialog once the plugin is enabled.
+
+
 tool
 extends EditorPlugin
 
